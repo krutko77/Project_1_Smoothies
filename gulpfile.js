ead0e6f0ba8542912a3f47be65687efd68b0c68a
Pavel <krutko77@mail.ru>
2021-07-24 12:46:11 +0300

    transitions

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,9 @@ const path = {
 // Если нужно выполнять преобразование файлов в определенном порядке, то используем массив с нужным нам порядком:
 const jsFiles = [
     srcPath + 'assets/js/lib.js',
-    srcPath + 'assets/js/main.js'
+    srcPath + 'assets/js/main.js',
+    srcPath + 'assets/js/wow.js',
+    srcPath + 'assets/js/vanilla-tilt.js'
 ]
 
 
