0aa8b39837fee28857b208f0ddc6203d6677eade
krutko77 <70036691+krutko77@users.noreply.github.com>
2022-03-13 20:23:21 +0300

    header corrected

---
+++ src/js/files/main.js
@@ -0,0 +1,10 @@
+
+import $ from "jquery";
+import "slick-carousel";
+
+
+// slick slider настройка
+$('.carousel').slick({
+      arrows:true,
+      dots:false
+   });
---
+++ src/js/files/script.js
@@ -0,0 +1,50 @@
+
+   // slick слайдер
+   $(document).ready(function(){
+      $('.slider').slick({
+         arrows:true         
+      });
+    })
+
+
+
+// бургер меню
+const menuIcon = document.querySelector('.header__menu-icon');
+const headerMenu = document.querySelector('.header__menu');
+   if (menuIcon) {      
+      menuIcon.addEventListener("click", function(){
+         document.body.classList.toggle('_lock');
+         menuIcon.classList.toggle('_active');
+         headerMenu.classList.toggle('_active');
+      });
+   }
+
+// прокрутка при клике на пункт меню
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset 
+         
+			if (menuIcon.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				menuIcon.classList.remove('_active');
+				headerMenu.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
+
